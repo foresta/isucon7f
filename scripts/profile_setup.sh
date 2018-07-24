c6056654f405e4aa6fc6af390376575d01403bbc
kazuki morita <kz.morit@gmail.com>
2018-07-24 19:45:13 +0900

    profile_setupからslow-queryを外す

--- scripts/profile_setup.sh
+++ scripts/profile_setup.sh
@@ -7,12 +7,12 @@ cd `dirname $0`
 . ./env.sh
 
 on() {
-    sh ./profile/slow_query.sh on
+#    sh ./profile/slow_query.sh on
     sh ./profile/nginx_for_kataribe.sh on
 }
 
 off() {
-    sh ./profile/slow_query.sh off
+#    sh ./profile/slow_query.sh off
     sh ./profile/nginx_for_kataribe.sh off
 }
 
